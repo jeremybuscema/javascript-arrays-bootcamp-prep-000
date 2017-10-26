98806bc452a0a2cf1fa56eec6fc187226fdbc476
Jeremy Buscema <jeremybuscema@gmail.com>
2017-10-26 23:20:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,6 @@ var chocolateBars = [
 function addElementToBeginningOfArray(array, element) {
   var newArray = ["element1", "element2"]
   newArray.unshift("element0");
-  return array;
+  return newArray;
 }
     
