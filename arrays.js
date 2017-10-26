3a190bfcdd609af13efe0482c0b9751ee40e890e
Jeremy Buscema <jeremybuscema@gmail.com>
2017-10-26 23:18:47 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+
+
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = ["element1", "element2"]
+  newArray.unshift("element0");
+    return element;
+
+}
