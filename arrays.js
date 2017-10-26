4ecff50a757db6946fbc43e46c0f76864faa8030
Jeremy Buscema <jeremybuscema@gmail.com>
2017-10-26 23:42:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,9 @@ function addElementToBeginningOfArray(array, element) {
   newArray.unshift("foo", 1);
   return newArray;
 }
+
+function addElementToEndOfArray(array, element) {
+  var array = []
+  array.push("foo", 1)
+  return array;
+}
